8ba5b9560f7d3b6d2c12187b44cf9e4272b0f8d5
DiplexSnake <hunteradelson@gmail.com>
2024-07-19 19:22:22 -0400

    Update Menu.gd
    Added full function to main game scene

--- Scenes/Mainmenu/Menu.gd
+++ Scenes/Mainmenu/Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/Start.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene("res://Scenes/Levels/node_3d.tscn")
+	get_tree().change_scene_to_file("res://node_3d.tscn")
 	
 
 
