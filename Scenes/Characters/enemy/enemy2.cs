e63db153a9e5bac2a3023c0f9ff381fd0795f7c5
DiplexSnake <hunteradelson@gmail.com>
2024-07-20 11:16:24 -0400

    gun fix

--- Scenes/Characters/enemy/enemy2.cs
+++ Scenes/Characters/enemy/enemy2.cs
@@ -3,75 +3,7 @@ using System;
 
 public partial class enemy2 : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	
-	public const float JumpVelocity = 4.5f;
 
-	public const float AttackRange = 10.0f;
 
-	public AnimationPlayer anim;
-	
-	public Node3D target;
-
-	public float gravity = -9.85f;
-	
-	public Vector3 velocity;
-
-	
-
-	public enum State{ 
-		IDLE,
-		Attack,
-		Struggle,
-		DIE
-		
- 	}
-
-	public State CurrentState;
-
-	public override void _Ready()
-	{
-		CurrentState = State.IDLE;
-		target = GetParent().GetNode<Node3D>("Player");
-		anim = GetNode<AnimationPlayer>("AnimationPlayer");
-
-		// searchArea.BodyEntered += MoveToward;
-	}
-	
-	
-	public override void _PhysicsProcess(double delta)
-	{
-		switch(CurrentState){
-			case State.IDLE:
-			Updateidle();
-			break;
-			case State.Attack:
-			UpdateAttack(delta);
-			break;
-
-		}
-	}
-
-	private void Updateidle(){
-		if (Position.DistanceTo(target.Position)> AttackRange)
-		{
-			CurrentState = State.Attack;
-		}
-	}
-
-	public void UpdateAttack(double delta)
-	{
-		Vector3 t = target.Position;
-		t.Y = Position.Y;
-		Vector3 direction = Position.DirectionTo(t);
-		velocity = Velocity;
-		velocity.X = direction.X * Speed;
-		velocity.Y = direction.Y * Speed;
-
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-	
-	
 
 }
