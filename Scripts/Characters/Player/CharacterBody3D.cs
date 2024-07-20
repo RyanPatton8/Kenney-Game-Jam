174a708ee837067e04bd82952f4ccafd5fcf824e
DiplexSnake <hunteradelson@gmail.com>
2024-07-19 20:10:44 -0400

    igonore

--- Scripts/Characters/Player/CharacterBody3D.cs
+++ Scripts/Characters/Player/CharacterBody3D.cs
@@ -8,19 +8,19 @@ public partial class CharacterBody3D : Godot.CharacterBody3D
 	public const float JumpVelocity = 4.5f;
 
 	[Export] public float MouseSensitivity = 0.01f;
-    [Export] public float MoveSpeed = 5.0f;
+	[Export] public float MoveSpeed = 5.0f;
 	[Export] public Node3D neck {get; private set;}
 	[Export] public Camera3D camera  {get; private set;}
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
-    public override void _PhysicsProcess(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
 
@@ -50,9 +50,9 @@ public partial class CharacterBody3D : Godot.CharacterBody3D
 		Velocity = velocity;
 		MoveAndSlide();
 	}
-    public override void _Input(InputEvent @event)
-    {
-        if(@event is InputEventMouseMotion)
+	public override void _Input(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion)
 		{
 			InputEventMouseMotion mouseMotion = @event as InputEventMouseMotion;
 			neck.RotateY(-mouseMotion.Relative.X * MouseSensitivity);
@@ -62,8 +62,8 @@ public partial class CharacterBody3D : Godot.CharacterBody3D
 			cameraRot.X = Mathf.Clamp(cameraRot.X, Mathf.DegToRad(-80f), Mathf.DegToRad(80f));
 			camera.Rotation = cameraRot;
 		}
-    }
-    public void RotateCamera()
+	}
+	public void RotateCamera()
 	{
 		
 	}
