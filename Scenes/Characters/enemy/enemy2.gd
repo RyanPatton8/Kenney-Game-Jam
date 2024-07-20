05b3f3696019f7c9f5c3a50c79a2b900f2d6b1aa
DiplexSnake <hunteradelson@gmail.com>
2024-07-20 18:17:28 -0400

    Added animations

--- Scenes/Characters/enemy/enemy2.gd
+++ Scenes/Characters/enemy/enemy2.gd
@@ -3,10 +3,16 @@ extends CharacterBody3D
 var player = null
 
 const SPEED = 4.0
+const ATTACK_RANGE = 2.5
+
+signal player_hit
 
 @export var player_path : NodePath
 
 @onready var nav_agent = $NavigationAgent3D
+@onready var anim_tree = $AnimationTree
+
+
 
 func _ready():
 	player = get_node(player_path)
@@ -19,9 +25,21 @@ func _process(delta):
 	
 	nav_agent.set_velocity(velocity)
 	look_at(Vector3(player.global_position.x, player.global_position.y, player.global_position.z), Vector3.UP)
+	
+	anim_tree.set("parameters/conditions/Attack", _target_in_range())
+	anim_tree.set("parameters/conditions/run", !_target_in_range())
 
+
+
+func _target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
 
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity):
 	velocity = velocity.move_toward(safe_velocity, .25)
 	move_and_slide()
+	
+func _hit_finished():
+	player.hit()
+
+
